a50d0d1bd563aca9d085656d498cfdaf27c3a774
Alexander Osborn <misterzander@gmail.com>
2018-08-27 19:36:01 -0500

    It kinda looks better

---
+++ assets/scripts/main.js
@@ -0,0 +1,10 @@
+var hangman = {
+    wins: 0,
+    losses: 0,
+    wordbank: ["football", "basketball", "hockey", "golf",
+                "soccer", "touchdown", "slapshot", "foul",
+                "penalty", "safety", "referee", "dunk",
+                "hook", "slice", "basket", "club",
+                "baseball", "homerun", "sideline", "bench",
+                "coach"]
+};
